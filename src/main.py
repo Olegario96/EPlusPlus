2f7676d4f6cb485c42aab09be67b72cce09994b3
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-05-04 15:54:48 -0300

    Delete main.py

--- src/main.py
+++
@@ -1,6 +0,0 @@
-from model.platformManager import PlatformManager
-from test.testPlatformManager import TestPlatformManager
-
-testPM = TestPlatformManager()
-pM = PlatformManager()
-testPM.testCheckAndInstallWindows(pM)
