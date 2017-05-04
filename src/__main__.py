75186aa78d933a98a0080061922a41411b157e7a
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-05-04 16:00:27 -0300

    Finishing installation tests on Windows

--- src/__main__.py
+++ src/__main__.py
@@ -4,4 +4,4 @@ from eplusplus.exception import InstallException
 
 testPM = TestPlatformManager()
 pM = PlatformManager()
-#testPM.testCheckAndInstallWindows(pM)
+testPM.testCheckAndInstallWindows(pM)
