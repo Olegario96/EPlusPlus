91add10b4dba804bd857a9d7e570136e68bbc448
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-05-05 17:12:26 -0300

    Update installer.py

--- src/model/installer.py
+++ src/model/installer.py
@@ -18,12 +18,13 @@ class Installer(object):
 		filename = "EnergyPlusInstaller.exe"
 		urllib.request.urlretrieve(url, filename)
 		path = os.path.abspath("EnergyPlusInstaller.exe")
-		subprocess.Popen(path, stdout=csv, shell=True)
-
+		print("foi")
+		subprocess.Popen(path, shell=True)
+		print("ta voando")
 
 	def installDBrowserWindows(self):
 		url = "https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.9.1/DB.Browser.for.SQLite-3.9.1-win64.exe"
 		filename = "sqlitebrowserInstaller.exe"
 		urllib.urlretrieve(url, filename)
 		path = os.path.abspath("sqlitebrowserInstaller.exe")
-		subprocess.Popen(path, stdout=csv, shell=True)
+		subprocess.Popen(path, shell=True)
