6c5083b457102cb64fd92e0eb3ab22b40cfd7225
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-05-09 18:10:51 -0300

    Updating documentation

--- src/exception/installException.py
+++ src/exception/installException.py
@@ -1,2 +1,8 @@
+##
+## @brief      Exception for signaling install errors.
+##             This exception is thrown whenever the eplusplus try to install
+##             some tool like the EnergyPlus or the DB browser for SQLite and
+##             didn't achieve the sucess.
+##
 class InstallException(Exception):
 	pass
