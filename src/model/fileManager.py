f14d2bd48a8f5984610a62dce9505d63375c7270
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-05-16 15:24:40 -0300

    Updating documentation. Not finished yet

--- src/model/fileManager.py
+++ src/model/fileManager.py
@@ -1,17 +1,22 @@
 import os
 import csv
-from .statiscal import Statiscal
 from eplusplus.exception.columnException import ColumnException
 
 ##
-## @brief
+## @brier This class is responsible for get the values from the csv file
+##        informed by the user and also to write the new values into the idf
+##        files. This class transform the values from the csv into a list
+##        of lists which list represents a column from the csv, following the
+##        order. The import of the os is used to get the name of the idf file.
+##        The csv library is needed cause we need to open the csv. The
+##        ColumnException is used when the csv and idf file doesn't match
+##        (i.e. the idf file require values that are in the csv file.)
+##
 ##
 class FileManager(object):
 
 	def __init__(self):
 		super(FileManager, self).__init__()
-		self.statiscal = Statiscal()
-
 
 	##
 	## @brief      This method reads the csv informed by the user. In the
@@ -44,41 +49,6 @@ class FileManager(object):
 				columns = [[] for i in range(len(row))]
 
 		return columns
-
-
-	##
-	## @brief      This method take the columns received from the
-	##             "columnsToLists" method and then apply a sampling method
-	##             on the the list based on the number of samples. For example
-	##             if we have a sample with 5 values and the "numSamples" is
-	##             equal to 3, this means that after apply the sampling method
-	##             we will have a list with 3 values choosed from the inital
-	##             sample.
-	##
-	## @param      self        Non static method
-	## @param      sample     Columns returned from the "columnsToLists"
-	##                         method. See its documentation for more info.
-	##
-	## @param      numSamples  Number of values that the user wants from the
-	##                         the sample. If this value is equal to 2, then
-	##                         the returned sample will have 2 elements. If 3,
-	##                         will return a list of 3 and so go on.
-	##
-	## @param      method      Method choosed by the user to apply the sampling
-	##                         method. In the first version of the eplusplus can
-	##                         be just two values: RANDOM or LHS.
-	##
-	## @return     The values returned from the sampling method applied. The
-	##             length of the list must be equal to the "numSamples" value.
-	##
-	def calculateNewValues(self, sample, numSamples, method):
-		for i in range(0, len(sample)):
-			if method == "LHS":
-				sample[i] = self.statiscal.lhsValues(sample[i], numSamples)
-			elif method == "RANDOM":
-				sample[i] = self.statiscal.randomValues(sample[i], numSamples)
-
-		return sample
 
 	##TODO
 	def writeNewValues(self, sample, pathToCsv, pathToIdf, pathToFolder):
