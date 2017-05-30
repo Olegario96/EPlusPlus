c1aeb514ad8aa398e3c0f8e6c28b3eedab6b0056
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-05-30 19:45:56 -0300

    Finishing documentation

--- src/view/lineEditDialog.py
+++ src/view/lineEditDialog.py
@@ -1,11 +1,26 @@
-import os
 from PyQt5.QtWidgets import QLineEdit, QFileDialog
 
+##
+## @brief      This class is a specialization of the class "QLineEdit". We use
+##             the specialization to override the method "keyPressEvent". This
+##             allow to every time that the user tries to type at the LineEdit
+##             nothing will happen.
+##
 class LineEditDialog(QLineEdit):
 	def __init__(self, parent):
 		super(LineEditDialog, self).__init__()
 		self.parentWindow = parent
 
+
+	##
+	## @brief      This method is a override. Every time the user tries to type
+	##             something at the line Edit, nothing will happen.
+	##
+	## @param      self   Non static mehtod
+	## @param      event  Event received from the Keyboard
+	##
+	## @return     This is a void function
+	##
 	def keyPressEvent(self, event):
 		None
 
