8cacc2d746298787b2a160a7e097f1a73d1ebd97
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-06-19 09:27:18 -0300

    Exception for the simulation. Already documented

---
+++ src/exception/noIdfException.py
@@ -0,0 +1,7 @@
+##
+## @brief      This exception is used for signaling that no IDF file were
+##             founded at folcer informed by the user. This exception is
+##             raised during simulation execution.
+##
+class NoIdfException(Exception):
+	pass
