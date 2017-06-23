f9390498d2f1f1c1f71bec4f82c15e3e7890d4cf
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-06-23 17:55:54 -0300

    Need it for compilation

---
+++ src/setup.py
@@ -0,0 +1,56 @@
+from distutils.core import setup
+from glob import glob
+import os
+import py2exe
+import pyDOE
+
+VERSION=1.0
+
+includes = [
+	"sip",
+	"PyQt5",
+	"PyQt5.QtCore",
+	"PyQt5.QtGui",
+	"PyQt5.QtWidgets",
+	"scipy.linalg.cython_blas",
+	"scipy.linalg.cython_lapack",
+	"pyDOE"
+]
+
+platforms = ["C:\\Python34\\Lib\\site-packages\\PyQt5\\plugins" +
+		"\\platforms\\qwindows.dll"]
+
+dll = ["C:\\windows\\syswow64\\MSVCP100.dll",
+		"C:\\windows\\syswow64\\MSVCR100.dll"]
+
+images = ["C:\\Users\\GUSTAVO\\EPlusPlus\\media\\img\\title.png",
+            			"C:\\Users\\GUSTAVO\\EPlusPlus\\media\\img\\icon.png"]
+
+documents = ["C:\\Users\\GUSTAVO\\EPlusPlus\\docs\\Documento"+
+				"RequisitosEplusplus.pdf"]
+
+datafiles = [("platforms", platforms),
+             ("", dll),
+             ("Images", images),
+             ("Documents", documents)]
+
+imageformats = glob("C:\\Python34\\Lib\\site-packages\\PyQt5\\"+
+					"plugins\\imageformats\\*")
+
+datafiles.append(("imageformats", imageformats))
+
+setup(
+	name="eplusplus",
+	version=VERSION,
+	packages=["eplusplus"],
+	url="",
+	license="",
+	windows=[{"script": "eplusplus/__main__.py"}],
+	scripts=[],
+	data_files = datafiles,
+	options={
+		"py2exe": {
+			"includes": includes,
+		}
+	}
+)
