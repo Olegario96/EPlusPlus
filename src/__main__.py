36cbacd7176ddb5d93051e548fb02940b68d79cd
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-07-06 14:59:02 -0300

    Delete __main__.py

--- src/__main__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from eplusplus.view import MainWindow
-
-args = list(sys.argv)
-app = QApplication(args)
-mainWindow = MainWindow(args)
-sys.exit(app.exec_())
