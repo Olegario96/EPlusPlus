a156a0da26619aeec7931fc0715e89454b790fd4
Gustavo Figueira Olegário <gustavo.olegario96@gmail.com>
2017-07-07 14:07:52 -0300

    Delete noIdfException.py

--- src/eplusplus/exception/noIdfException.py
+++
@@ -1,7 +0,0 @@
-##
-## @brief      This exception is used for signaling that no IDF file were
-##             founded at folcer informed by the user. This exception is
-##             raised during simulation execution.
-##
-class NoIdfException(Exception):
-	pass
