d7bf3dbaa0451620a8f4d33fa72a4c7b3d952667
Gustavo Figueira Olegário <gustavo-olegario@hotmail.com>
2017-08-01 17:51:58 -0300

    Adding the idfNotFormatted import

--- eplusplus/exception/__init__.py
+++ eplusplus/exception/__init__.py
@@ -1,3 +1,4 @@
 from .installException import InstallException
 from .columnException import ColumnException
 from .noIdfException import NoIdfException
+from .idfNotFormatted import IdfNotFormatted
